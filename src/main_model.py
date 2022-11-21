2027a65bbfa0643e998734233fd8c043e07112c0
annechoww <111619072+annechoww@users.noreply.github.com>
2022-11-21 18:54:16 -0500

    loaded data into an array in proper arrays

--- src/main_model.py
+++ src/main_model.py
@@ -15,15 +15,6 @@ import torchvision.transforms as transforms
 # check device configuration
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-# define hyperparameters
-input_size = 9
-hidden_size = 500
-num_classes = 5
-num_epochs = 5
-batch_size = 100
-learning_rate = 0.0001
-
-
 # Load Data
 # Helper Music_Data Class
 class Music_Data:
@@ -46,14 +37,27 @@ class Music_Data:
                     reader = csv.reader(fd, delimiter=',')
                     count = 0
                     for row in reader:
-                        row.append(num)
+                        if row[0] == 'danceability':
+                            continue
+                        col_idx = [0,1,3,6,9,10]
+                        temp_row = []
+                        for col in col_idx:
+                            if(row[col] == "{'status': 401, 'message': 'The access token expired'}"):
+                                print(file)
+
+                            temp_row.append(float(row[col]))
+
+                        temp_row.append(num)
                         if count > 0 and row[0] != '':
-                            data_orig.append(row)
+                            data_orig.append(temp_row)
                         count += 1
 
-        print(data_orig)
+        # print(data_orig)
         # converts input data into a tensor
-        self.data = torch.Tensor(data_orig)
+        self.data = list(data_orig)
+        # self.data = torch.Tensor(data_orig)
+        #print(self.data)
+        # self.keep_columns([0, 1, 3, 6, 9, 10, 18])
         
     def keep_columns(self, L):
         """Select Features """           
@@ -79,12 +83,6 @@ class fcn(nn.Module):
         out = self.relu(out)
         out = self.fc2(out)
         return out
-
-model = fcn(input_size, hidden_size, num_classes).to(device)
-
-# Loss and optimizer
-criterion = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)  
 
 def get_accuracy(truth, pred):
     assert len(truth)==len(pred)
@@ -157,18 +155,20 @@ if __name__ == "__main__":
     # call function to prepare data structure
     pathname = os.getcwd()
     train_data = Music_Data(pathname + '\input_data\\train') 
-    val_data = Music_Data(pathname + '\input_data\val')
+    val_data = Music_Data(pathname + '\input_data\\val')
     test_data = Music_Data(pathname + '\input_data\\test')
     title = ['danceability', 'energy', 'loudness', 'acousticness', 'valence', 'tempo', 'mood']
 
-    # remove unnecessary columns and convert array to float
-    train_feature_data = train_data.keep_columns([0, 1, 3, 6, 9, 10, 18]) # dance, energy, loudness, acousticness, valence, tempo, mood
-    #print(train_feature_data[0])
-    val_feature_data = val_data.keep_columns([0, 1, 3, 6, 9, 10, 18])
-    test_feature_data = test_data.keep_columns([0, 1, 3, 6, 9, 10, 18])
-    #print(feature_data[0:5,:])    
-    df_train = pd.DataFrame(train_feature_data, columns = title)
-    df_test = pd.DataFrame(test_feature_data, columns = title)
-    print(df_train)
-    print(df_test)
     moods = ['Angry', 'Calm', 'Happy', 'Love', 'Sad']
+
+    # print(train_data)
+    # define hyperparameters
+    input_size = 9
+    hidden_size = 500
+    num_classes = 5
+    num_epochs = 5
+    batch_size = 100
+    learning_rate = 0.0001
+
+
+    model = fcn(input_size, hidden_size, num_classes)
