bc993eeb9648f0900c6cb1c11f45bf79629bf0b3
abeerfatima <abeerfatima182@gmail.com>
2022-12-01 17:09:42 -0500

    tuned n_estimators in baseline.py

--- src/baseline.py
+++ src/baseline.py
@@ -36,7 +36,6 @@ class Music_Data:
                 df = pd.read_csv(file)
                 with open(file) as fd:
                     reader = csv.reader(fd, delimiter=',')
-                    #df[['loudness','tempo']] = (df[['loudness','tempo']] - df[['loudness','tempo']].min())/(df[['loudness','tempo']].max() - df[['loudness','tempo']].min())
                     count = 0
                     for row in reader:
                         row.append(num)
@@ -60,7 +59,6 @@ if __name__ == "__main__":
     pathname = os.getcwd()
     train_data = Music_Data(pathname + '\src\input_data\\train') # separate??
     #train_nums = train_data.data[1:]
-    #val_data = Music_Data(pathname + '\src\input_data\val')
     test_data = Music_Data(pathname + '\src\input_data\\test')
     #print(train_nums)
     #test_nums = test_data[1:]
@@ -69,7 +67,6 @@ if __name__ == "__main__":
     # remove unnecessary columns and convert array to float
     train_feature_data = train_data.keep_columns([0, 1, 3, 6, 9, 10, 18]) # dance, energy, loudness, acousticness, valence, tempo, mood
     #print(train_feature_data[0])
-    #val_feature_data = val_data.keep_columns([0, 1, 3, 6, 9, 10, 18])
     
     test_feature_data = test_data.keep_columns([0, 1, 3, 6, 9, 10, 18])
     #print(feature_data[0:5,:])    
@@ -80,28 +77,6 @@ if __name__ == "__main__":
     # print(df_test)
     moods = ['Angry', 'Calm', 'Happy', 'Love', 'Sad']
 
-    #model = DecisionTreeClassifier(max_depth=10)
-    # model = RandomForestClassifier(n_estimators=50)
-    # train_labels = train_feature_data[0:,6]
-    # train_features = train_feature_data[0:,0:5]
-    # #val_labels = val_feature_data[0:,6]
-    # #val_features = val_feature_data[0:,0:5]
-    # # # # Fit the model to our training data
-    # model.fit(train_features, train_labels)
-    # training_predicted = model.predict(train_features)
-    # score = (1-sum(abs(training_predicted-train_labels))/len(training_predicted)) # LMAOOOO WHAT
-    # print('training accuracy:', score)
-
-    # model.fit(val_features, val_labels)
-    # #val_predicted = model.predict(val_features)
-    # score = (1-sum(abs(val_predicted-val_labels))/len(val_predicted)) # LMAOOOO WHAT
-    # print('validation accuracy:', score)
-    # # # Make predictions
-    # test_labels = test_feature_data[0:,6]
-    # test_features = test_feature_data[0:,0:5]
-    # testing_predicted = m.predict(test_features)
-    # score = (1-sum(abs(testing_predicted-test_labels))/len(testing_predicted)) # LMAOOOO WHAT
-    # print('testing accuracy:', score)
 
     X_train, X_test, y_train, y_test = train_test_split(df.drop(columns = ["mood"]),
                                                             df.mood,
@@ -115,8 +90,8 @@ if __name__ == "__main__":
         train_score = mdl.score(X_train, y_train)
         test_score = mdl.score(X_test, y_test)
 
-        print('the accuracy on the: \n\t training data is {}'.format(round(train_score,3)))
-        print('\ttesting data is {}'.format(round(test_score,3)))
+        print('Accuracy for \n\t training data: {}'.format(round(train_score,3)))
+        print('\ttesting data: {}'.format(round(test_score,3)))
         return train_score, test_score
     print('CART Model:')
     cart_model = DecisionTreeClassifier(random_state= 1, max_depth = 10)
@@ -127,6 +102,18 @@ if __name__ == "__main__":
     print('Random Forest:\n')
     for i in depths:
         mdl = RandomForestClassifier(n_estimators = 100, max_depth = i, random_state = 1, bootstrap = True, max_samples = 0.2)
-        print(f'the accuracy on {i} max depth:')
+        print(f'max_depth {i}:')
         train_score, test_score = fit_and_score_model(mdl, X_train, X_test, y_train, y_test, random_state = 1)
         scores.append(test_score)
+
+    n_e = [100,150,200,250,300,350]
+    scores = []
+    print('Random Forest:\n')
+    for i in n_e:
+        mdl = RandomForestClassifier(n_estimators = i, max_depth = 10, random_state = 1, bootstrap = True, max_samples = 0.2)
+        print(f'{i} n_estimators:')
+        train_score, test_score = fit_and_score_model(mdl, X_train, X_test, y_train, y_test, random_state = 1)
+        scores.append(test_score)
+
+    # confusion matrix
+    
